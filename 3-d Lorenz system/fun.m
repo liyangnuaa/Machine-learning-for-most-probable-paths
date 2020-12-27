ad93e3aa33f5cfc0d96f6ec86ea3549ac4c8db0b
liyangnuaa <61436455+liyangnuaa@users.noreply.github.com>
2020-12-27 14:36:47 +0800

    Create fun.m

--- 3-d Lorenz system/fun.m
+++ 3-d Lorenz system/fun.m
@@ -1,6 +1,6 @@
 function y=fun(~,x)
 
-global c dab1 dab2 dab3 sigma beta rou
+global dab1 dab2 dab3 sigma beta rou kapa miu
 
 x1f=x(1);
 x2f=x(2);
@@ -10,9 +10,12 @@ p2f=x(5);
 p3f=x(6);
 
 y=zeros(size(x));
-y(1)=2*p1f*c^2 - 2*dab1 - 2*sigma*(x1f - x2f);
-y(2)=2*p2f*c^2 - 2*dab2 - 2*x2f + 2*rou*x1f - 2*x1f*x3f;
-y(3)=2*p3f*c^2 - 2*dab3 - 2*beta*x3f + 2*x1f*x2f;
-y(4)=-(2*p3f*x2f - 2*p1f*sigma + p2f*(2*rou - 2*x3f));
-y(5)=-(2*p1f*sigma - 2*p2f + 2*p3f*x1f);
-y(6)=-(- 2*beta*p3f - 2*p2f*x1f);
+y(1)=p1f/2 - dab1 - sigma*(x1f - x2f);
+y(2)=p2f/2 - dab2 - x2f + rou*x1f - x1f*x3f;
+y(3)=x1f*x2f - beta*x3f - dab3 + (p3f*(miu*x3f^2 + 1))/2 + miu*x3f*(kapa - 1/2);
+y(4)=-(p3f*x2f - p1f*sigma + p2f*(rou - x3f) + (miu*x2f*x3f)/(miu*x3f^2 + 1));
+y(5)=-(p1f*sigma - p2f + p3f*x1f + (miu*x1f*x3f)/(miu*x3f^2 + 1));
+y(6)=-((miu*p3f^2*x3f)/2 - p2f*x1f - p3f*(beta - miu*(kapa - 1/2)) +...
+    (miu*(x1f*x2f - beta*x3f + miu*x3f*(kapa - 1/2)))/(miu*x3f^2 + 1) -...
+    (miu*x3f*(beta - miu*(kapa - 1/2)))/(miu*x3f^2 + 1) -...
+    (2*miu^2*x3f^2*(x1f*x2f - beta*x3f + miu*x3f*(kapa - 1/2)))/(miu*x3f^2 + 1)^2 - (2*dab3^2*miu*x3f)/(miu*x3f^2 + 1)^2);
